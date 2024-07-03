466d85065ec13f089f0fe54afd1af719212b97e5
giordanofabrizii <giordanofabrizi@gmail.com>
2024-07-03 15:29:04 +0200

    fixed classes with parenting

--- utilities/db.php
+++ utilities/db.php
@@ -1,1 +1,13 @@
-<?php  ?>
+<?php  
+
+// > Categorie
+$accessorio = new 
+
+// # Animali
+$cane = new Animale('fa-solid fa-dog','Cane');
+$gatto = new Animale('fa-solid fa-cat','Gatto');
+$pesce = new Animale('fa-solid fa-fish','Pesce');
+$uccello = new Animale('fa-solid fa-dove','Uccello');
+
+// * Items
+$royal = new Cibo($cane, 'Royal Canin Mini Adult', '43,99','454g', ['Prosciutto','Riso']);
